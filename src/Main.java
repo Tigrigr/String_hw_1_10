5c4882ffe4bd09fee4aca3da72d9f40b00a8e63c
mikha <Mikhailov.dzenphone@gmail.com>
2023-04-18 13:54:08 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello rld!");
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Иван ";
+        String middleName = "Иванов ";
+        String lastName = "Иванович ";
+        String fullName = middleName + firstName + lastName;
+        System.out.println("Имя сотрудника - " + fullName);
+
     }
 }
