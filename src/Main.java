79ad0582228e24ac9997e95a6ee6369f63ee9879
mikha <Mikhailov.dzenphone@gmail.com>
2023-04-18 13:57:09 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
+
     public static void task1() {
         System.out.println("Задача 1");
-        String firstName = "Иван ";
-        String middleName = "Иванов ";
-        String lastName = "Иванович ";
+        String firstName = "Ivan ";
+        String middleName = "Ivanov ";
+        String lastName = "Ivanovich ";
         String fullName = middleName + firstName + lastName;
         System.out.println("Имя сотрудника - " + fullName);
 
+    }
+
+    public static void task2() {
+        System.out.println();
+        System.out.println("Задача 2");
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println(fullName.toUpperCase());
     }
 }
